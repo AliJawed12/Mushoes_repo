c52c8db3bf5bdb805cc09c88d0835327ec17626a
Ali Jawed <alijawed12@hotmail.com>
2025-08-18 13:29:35 -0400

    Created Basic template of site. Header and cart sidebar. Added basic cart functionality in javascript: open & close cart.

---
+++ scripts/cart.js
@@ -0,0 +1,19 @@
+
+window.onload = function() {
+  
+  const openButton = document.getElementById("cart-text-button");
+  const closeButton = document.getElementById("cart-close-button");
+  const cartSideBar = document.getElementById("cart-sidebar");
+
+  openButton.addEventListener("click", () => {
+    cartSideBar.classList.add("open");
+    document.body.classList.add("no-scroll");
+  });
+
+  closeButton.addEventListener("click", () => {
+    cartSideBar.classList.remove("open");
+    document.body.classList.remove("no-scroll");
+  });
+
+
+};
