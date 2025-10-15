bfa385c510a68a5d213f8e7079d73e3f0d2309f4
Ali Jawed <alijawed12@hotmail.com>
2025-10-15 16:05:22 -0400

    Connected to cloudinary, upload mushoes logo for image hosting.
    cloudinary-connection.js is a mandatory file which exports cloudinary for use.
    
    testUpload.js is a temp file used to test connection and uploading of image

---
+++ node_quickstart/cloudinary-connection.js
@@ -0,0 +1,17 @@
+/* cloudinary-connection.js */
+
+/* Connects to image hosting site, Cloudinary */
+
+import {v2 as cloudinary} from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure: true
+});
+
+export default cloudinary;
---
+++ node_quickstart/testUpload.js
@@ -0,0 +1,17 @@
+/* testupload.js */
+
+import cloudinary from "./cloudinary-connection.js";
+
+const uploadImage = async () => {
+  try {
+    const result = await cloudinary.uploader.upload("../images/mushoes-icon.PNG", {
+      folder: "mushoes-logos"
+    });
+
+    console.log("Image Uploaded", result.secure_url);
+  } catch (error) {
+    console.log("upload failed ", error);
+  }
+};
+
+uploadImage();
